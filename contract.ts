9f229cb6ca4f12338c6024534037b846f1a1b91f
Renan Costa <renancostam@gmail.com>
2024-08-27 15:29:31 -0300

    Refactoring Contract type

--- contract.ts
+++ contract.ts
@@ -12,16 +12,28 @@ export type Template = {
   createdAt: Timestamp;
 };
 
+export type IntegerString = `${number}` & `${bigint}`;
+
 export type Contract = {
+  /** The name that identifies the type of contract. */
   name: string;
+  /** Data object sent via forms */
   data: object | null;
+  /** Array with IDs of users who can access the document */
   ownersUids: string[];
+  /** The contract template object */
   template?: Template;
+  /** The docx object to generate the contract file */
   docxDocument?: Document;
   documentUpdatedAt?: Timestamp;
+  /** Array of DocumentChange objects created in review process */
   changes?: DocumentChange[];
   status?: "draft" | "completed";
-  draftStage?: "forms" | "review";
+  /** Contract drafting stage. Accepted values "welcome", "review" and any integer number as string, to represents the last forms index fulfilled  */
+  draftStage?: "welcome" | IntegerString | "review";
+  /** Contract price charged to regular user */
+  value?: number;
+  /** Access token used in the contract sharing flow between users */
   accessToken?: string;
   createdAt: Timestamp;
   updatedAt?: Timestamp;

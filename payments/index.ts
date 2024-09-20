be39ed10731ce9f3b4090288808785e4ab8b598d
Pedro Andrade <pdandradeb@gmail.com>
2024-09-20 16:55:37 -0300

    Adding billingType to PayRequest

--- payments/index.ts
+++ payments/index.ts
@@ -1,15 +1,17 @@
 import { Timestamp } from "../external";
 import { BaseRequest, ErrorResponse } from "../functions";
-import { AssasPayment } from "./assas";
+import { AssasBillingType, AssasPayment } from "./assas";
 
 export interface PayRequest extends BaseRequest {
   contractId: string;
+  billingType: PaymentBillingType;
 }
 
 export interface PaySuccessResponse {}
 
 export type PayResponse = PaySuccessResponse | ErrorResponse;
 
+export type PaymentBillingType = AssasBillingType;
 export type PaymentProcessing = AssasPayment;
 
 export interface Payment {
